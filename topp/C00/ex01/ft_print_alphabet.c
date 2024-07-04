1244a165e374aa36b6a8cda249e163c814b1b307
TaukTauk <tauktauk51833@gmail.com>
2024-07-05 00:15:01 +0700

    exercise 01

---
+++ topp/C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void    ft_print_alphabet(void);
+
+int     main(void)
+{
+    ft_print_alphabet();
+}
+
+void    ft_print_alphabet(void)
+{
+    char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+}
